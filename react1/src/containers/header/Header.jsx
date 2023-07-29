f5d15ee00d8bf03d6d0d8c0c6c157756eb98e282
Silvia Chang <sunny1218.chang@gmail.com>
2023-07-29 14:11:03 +0800

    20230729

--- react1/src/containers/header/Header.jsx
+++ react1/src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div className='gqt3_header section_padding' id='home'>
+    <div className='gpt3_header section_padding' id='home'>
       <div className='gpt3_header-content'>
         <h1 className='gradient_text'>Let's build something amazing with GPT-3 OpenAI</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
@@ -17,10 +17,9 @@ const Header = () => {
           <img src={people} alt='people'/>
           <p>1,600 people requested access a visit in last 24 hours.</p>
         </div>
-
-        <div className='gpt3_header-image'>
-          <img src={ai} alt='ai'/> 
-        </div>
+      </div>
+      <div className='gpt3_header-image'>
+        <img src={ai} alt='ai'/>   
       </div>
     </div>
   )
