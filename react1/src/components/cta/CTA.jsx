220306db5046932595448f24ec4c2ae92291e9e5
Silvia Chang <sunny1218.chang@gmail.com>
2023-07-30 12:27:58 +0800

    blog

--- react1/src/components/cta/CTA.jsx
+++ react1/src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className='gpt3_cta'>
+      <div className='gpt3_cta-content'>
+        <p>Request Early Access To Get Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className='gpt3_cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
